e5c96e62a1d4820e2584e2e0b3bd63572d68e203
Vidul Nikolaev Petrov <vidul.petrov@gmail.com>
2020-06-30 05:20:06 +0300

    Add default settings' tests

---
+++ __tests__/test_default_settings.js
@@ -0,0 +1,36 @@
+'use strict';
+
+require('../index');
+
+const linkWrap = window.$linkWrap;
+
+document.body.innerHTML = `
+    <p id="anyUp" wrapper>
+        <a name="up"></a>
+        <a href="#down" wrapped>go down</a>
+    </p>
+    <div id="anyDown" wrapper>
+        <a name="down"></a>
+        <a href="#up" wrapped>go up</a>
+    </div>
+`;
+
+linkWrap.init();
+
+test('navigate down from any wrapper', done => {
+    document.getElementById('anyUp').click();
+
+    setTimeout(() => {
+        expect(window.location.href).toMatch(/down$/);
+        done();
+    });
+});
+
+test('navigate up from any wrapper', done => {
+    document.getElementById('anyDown').click();
+
+    setTimeout(() => {
+        expect(window.location.href).toMatch(/up$/)
+        done();
+    });
+});
